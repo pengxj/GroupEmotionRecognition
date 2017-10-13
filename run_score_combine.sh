1592cc8752a73fc6449d9e4aedd83a3d8ebd0335
pengxj <xiaojiangp@gmail.com>
2017-10-13 23:11:03 +0800

    add sh for 7 runs

---
+++ run_score_combine.sh
@@ -0,0 +1,13 @@
+python test_combine.py 1 test_scores/global_resnet101_trval.txt test_scores/faceExpw_tv_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.5 3.0 0.5
+
+python test_combine.py 2 test_scores/global_vgg19_trval.txt test_scores/faceExpw_tv_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.5 3.0 0.5
+
+python test_combine.py 3 test_scores/global_resnet101_trval.txt test_scores/faceExpw_t_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.5 3.0 0.5
+
+python test_combine.py 4 test_scores/global_vgg19_trval.txt test_scores/faceExpw_t_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.5 3.0 0.5
+
+python test_combine.py 5 test_scores/global_resnet101_trvalextra.txt test_scores/faceExpw_tv_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.8 3.2 0.6
+
+python test_combine.py 6 test_scores/global_vgg19_trvalextra.txt test_scores/faceExpw_tv_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.8 3.2 0.6
+
+python test_combine.py 7 test_scores/global_resnet101_trvalextra.txt test_scores/faceExpw_t_0.txt test_scores/non_aligned_face_res34_trval_top3.txt 0.8 3.2 0.6
